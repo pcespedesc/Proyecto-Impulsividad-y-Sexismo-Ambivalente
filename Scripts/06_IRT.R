6583a67758ebfed39add4bd767db40de2051b00e
Paula Céspedes <pcespedesc@unal.edu.co>
2026-06-05 21:14:51 -0500

    Update 06_IRT.R

--- Scripts/06_IRT.R
+++ Scripts/06_IRT.R
@@ -17,19 +17,22 @@ summary(fit_grm_eip)
 #parámetros
 params_grm <- coef(fit_grm_eip, IRTpars = TRUE, simplify = TRUE)
 round(params_grm$items, 3)
-
+#EIP04 y EIP06 tienen discriminaciones negativas, lo que confirma lo observado en el AFC. Así como EIP15 que tiene una discriminación muy baja. 
+#En EIP04, EIP06 y EIP11 los umbrales se comportan de manera anormal, lo que indica que los ítems no están funcionando bien. 
 #Gráficos
 plot(fit_grm_eip, type = "trace",
      main = "Curvas Características de los Ítems - EIP")
-
+# Ítems como el 02, 05 y 10 muestran curvas ordenadas en donde cada categoría domuna en un rango de theta, pero EIP06 y EIP11 muestran curvas casi planas y superpuestas confirmando que no discriminan. 
+# EIP04 muestra un patrón invertido. 
 plot(fit_grm_eip, type = "infotrace",
      main = "Información por Ítem - EIP")
-
+#EIP 04, 06, 09 y 11 aportan poca información o casi nula para estimar el rasgo, mientras que EIP02 y EIP10 son los más informativos complementados por EIP05 que aporta información sobre personas con poco nivel de rasgo 
 plot(fit_grm_eip, type = "info",
      main = "Información Total del Test - EIP")
-
+#Se puede ver que el test mide con mayor precisión a personas con impulsividad media-alta. 
 plot(fit_grm_eip, type = "SE",
      main = "Error Estándar - EIP")
+#El error mínimo se alcanza entre θ = -1 y 2, lo cual es consistente con el gráfico anterior.
 
 # GRM para la Escala de Sexismo Ambivalente
 
@@ -52,14 +55,21 @@ summary(fit_grm_hostil)
 #parámetros
 round(coef(fit_grm_hostil, IRTpars = TRUE, simplify = TRUE)$items,
   3)
-
+#Todos los ítems tienen discriminación positiva a diferencia de EIP. ESA11 es el que más discrimina, seguido de ESA14, ESA05 Y ESA10. Los ítems con discriminación más baja 
+#son el ESA04 y ESA18, que aunque distingan con menos precisión siguen funcionando. 
+#Asimismo, todos tienen umbrales ordenados, e incluso los umbrales de b1 son muy negativos lo que significa que incluso personas con niveles bajos de sexismo hostil tienen alta probabilidad
+#de responder en categorías distintas a la mínima, lo que va en concordancia con los descriptivos. 
+       
 #Gráficos
 
 plot(fit_grm_hostil, type = "trace",
      main = "CCI - Sexismo Hostil")
-
+#Todos los ítems muestran curvas ordenadas y separadas. SA14 y ESA05 tienen las curvas más bien definidas y separadas, consistente con sus altos parámetros de discriminación. 
+#ESA04 y ESA18 muestran curvas algo más superpuestas, coherente con su menor discriminación.
 plot(fit_grm_hostil, type = "info",
      main = "Información - Sexismo Hostil")
+#La curva de información tiene un rango amplio de theta entre -2 y 2 con un pico máximo cerca de 0, lo que indica que la escala mide con precisión a personas 
+#con niveles bajos como medios de sexismo hostil. 
 
 #Sexismo benevolente 
 #Categorías de respuesta de ítems 
@@ -79,63 +89,18 @@ summary(fit_grm_benev)
 #parámetros
 round(coef(fit_grm_benev, IRTpars = TRUE, simplify = TRUE)$items,
   3)
+#ESA12 es el ítem que más discrimina y tiene umbrales ordenados, siendo practicamente el que domina la medición del factor, lo cual es consistente con su carga de 0.92 en el AFC
+#ESA09, ESA22, ESA01 funcionan bien y el resto tienen discriminación baja pero positiva a excepción del ESA13 que su valor de a=-1.13 y umbrales invertidos lo que confirma por tercera vez que funciona en dirección opuesta.
+#ESA06 tiene discriminación casi nula, tambien consistente con lo observado anteriormente. Al igual que umbrales que se comportan de manera anómala.  
+#ESA09 Y ESA08 tienen umbrales muy negativos, lo que significa que se necesita un nivel muy bajo de sexismo benevolente para responder en la categoría mínima, por lo que casi toda la muestra tiende a responder en categorías medias o altas. 
 
 #Gráficos
 
 plot(fit_grm_benev, type = "trace",
      main = "CCI - Sexismo Benevolente")
-
+#Aquí se ve más heterogeneidad entre ítems. ESA12 tiene curvas muy juntas y con transiciones muy marcadas. ESA13 tiene un patrón invertido.
+#ESA06 tiene curvas casi planas y superpuestas. ESA01, ESA03, ESA17 y ESA22 muestran curvas ordenadas similares a las del sexismo hostil. 
 plot(fit_grm_benev, type = "info",
      main = "Información - Sexismo Benevolente")
-
-
-#8. PLS-SEM MODELO 1 (SEXISMO HOSTIL Y BENEVOLENTE)
-
-#Como el AFC ya validó la estructura factorial, en PLS-SEM se tratan las variables a partir de sus puntajes totales 
-
-#Se define el modelo y qué variables miden qué constructos
-medida_m1 <- constructs(
-  composite("Impulsividad", single_item("EIP")),
-  composite("SexHostil",    single_item("SEXHOS")),
-  composite("SexBenev",     single_item("SEXBENE"))
-)
-
-#Se define el modelo estructural
-
-#La impulsividad predice el sexismo hostil y el benevolente 
-estructura_m1 <- relationships(
-  paths(from = "Impulsividad", to = c("SexHostil", "SexBenev"))
-)
-
-modelo_m1 <- estimate_pls(
-  data = datos_m1,
-  measurement_model = medida_m1,
-  structural_model = estructura_m1
-)
-
-summary(modelo_m1)
-
-#R² indica el porcentaje de varianza del sexismo que explica la impulsividad
-
-#Bootstraping 
-boot_m1 <- bootstrap_model(modelo_m1,
-                           nboot = 1000,
-                           seed = 123)
-summary(boot_m1)
-
-#Capacidad predictiva (PLSpredict
-pred_m1 <- predict_pls(
-  model = modelo_m1,
-  technique = predict_DA,
-  noFolds = 10,
-  reps = 10
-)
-summary(pred_m1)
-
-#Gráfico modelo 1 
-plot(modelo_m1)
-plot(boot_m1)
-
-#RESULTADO: la impulsividad predice de manera positiva y estadísticamente significativa el sexismo hostil. 
-#sin embargo, solo explica el 5.6% de las diferencias individuales en sexismo hostil, por lo que el tamaño del efecto es pequeño.
-#En el caso del sexismo benevolente la impulsividad no predice significativamente, así que no se demuestra una relación entre ambas variables.
+#La curva muestra tres picos, lo que refleja la heterogeneidad de los ítems. La información se concentra en el rango negativo de θ (entre -2.5 y 0), lo que indica que la escala mide con mayor precisión a personas con niveles bajos a medios de sexismo benevolente. A partir de θ = 1 la información cae marcadamente, lo que significa que la escala tiene poca precisión para discriminar entre personas con niveles altos del rasgo. 
+#Comparado con el sexismo hostil, cuya curva era más simétrica, esta escala tiene cobertura más asimétrica del rasgo. 
